150b3f5cbb3eaf7660204a25a6097595c2779b1a
Niall Tyndall <ntyndall@gmail.com>
2018-07-09 14:54:33 +0100

    Create function for reading string and returning sorted tuple (plus setting up empty Leaf data structure

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 import Lib
+import Data.List
 
 main :: IO ()
 main = someFunc
+
+data Htree a = Leaf | Node a (Htree a) (Htree a) deriving (Show)
+
+-- |Take a string and group by element, then return sorted tuple
+freqCount :: Eq a => [a] -> [([a], Int)]
+freqCount x = sortBy (compare `on` snd) . map (\x -> ([head x], length x)) . group $ x
+
+-- |Create an empty Node
+emptyNode :: a -> Htree a
+emptyNode x = Node x Leaf Leaf 
+
