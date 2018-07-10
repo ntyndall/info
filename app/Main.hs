3e296e118968e9dfdec576cfd30a79bebe13fa0a
Niall Tyndall <ntyndall@gmail.com>
2018-07-10 14:13:38 +0100

    Get working flow for calculating and printing out huffman encoding

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,12 @@ import Data.Ord
 import Data.Function
 import qualified Data.Text.IO as T
 
-main :: IO ()
-main = return()
-
 data HTree a = Leaf a | Stem (HTree a) (HTree a) deriving (Show)
+
+main = do
+    putStrLn "Provide string to convert to Huffman encoding"
+    myStr <- getLine
+    putStrLn (hCodes myStr)
 
 -- |Take a string and group by element, then return sorted tuple
 freqCount :: Eq a => [a] -> [([a], Int)]
@@ -19,12 +21,15 @@ htree :: (Num a, Ord a) => [(a, HTree a1)] -> HTree a1
 htree [(_, t)] = t
 htree ((w1,t1):(w2,t2):wts) = htree $ insertBy (comparing fst) (w1 + w2, Stem t1 t2) wts
 
--- |Convert the stem and leaf structure to 0 or 1 for a hamming code
+-- |Convert the stem and leaf structure to 0 or 1 for a huffman code
 treeToCode :: HTree t -> [(t, [Char])]
 treeToCode (Stem a b) = 
-  [(x, '0':c) | (x, c) <- treeToCode a] ++ 
-  [(x, '1':c) | (x, c) <- treeToCode b]
+    [(x, '0':c) | (x, c) <- treeToCode a] ++
+    [(x, '1':c) | (x, c) <- treeToCode b]
 treeToCode (Leaf x) = [(x, "")]
 
--- Return list of hamming codes
-hCodes x = treeToCode $ htree $ sortBy (comparing fst) $ x
+-- Return list of huffman codes
+hCodes :: [Char] -> String
+hCodes x = unwords $ foldr1 (++) $ map (\(x,y) -> [x,y]) $
+    treeToCode $ htree $ sortBy (comparing fst) $
+    [(w, Leaf z) | (z, w) <- freqCount x]
